3c71ce98ee7fbee3702b0d31c7ae6321920bcebe
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-03-04 15:21:04 -0300

    Adicao de stilo para pagina, criacao de elementos para a pagina e exclui o q nn ia usar

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className='title'>Buscador CEP</h1>
+
+
+        <div className="containerInput">
+          <input 
+            type="text" 
+            placeholder="Digite seu CEP...">
+          </input>
+
+          <button className="buttonSearch">
+            <FiSearch size={25} color="#FFF"/>
+          </button>
+
+        </div>
+
+        <main className='main'>
+            <h2>CEP: 1234567</h2>
+
+            <span>Rua Teste</span>
+
+            <span>Complemento: teste complemento</span>
+
+            <span>Vila Rosa</span>
+
+            <span>Campo Grande - BA</span>
+          </main>
     </div>
   );
 }
